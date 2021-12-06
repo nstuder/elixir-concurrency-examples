2d1cbb0adcc05272ea7892b9bf31f861f6908652
Niklas Studer <niki.studer@web.de>
2021-12-06 12:40:22 +0100

    basic: add spawn Demo

--- basic/lib/basic.ex
+++ basic/lib/basic.ex
@@ -1,18 +1,26 @@
 defmodule Basic do
-  @moduledoc """
-  Documentation for `Basic`.
-  """
+  def main do
+    # spawn Process anonymous Function
+    pid = spawn(fn -> IO.puts("Hello World from Process") end)
+    Process.info(pid)
 
-  @doc """
-  Hello world.
+    # spawn Process with named Function
+    pid = spawn(Basic, :greet, [])
 
-  ## Examples
+    # send Messages to Process
+    send(pid, {:german, "Niklas"})
+    send(pid, {:english, "Niklas"})
+    send(pid, {:french, "Niklas"})
+  end
 
-      iex> Basic.hello()
-      :world
+  def greet do
+    receive do
+      {:german, name} -> IO.puts("Guten Tag #{name}")
+      {:english, name} -> IO.puts("Hello #{name}")
+      {_, name} -> IO.puts("Unknown Language: Name: #{name}")
+    end
 
-  """
-  def hello do
-    :world
+    # loop
+    greet()
   end
 end
