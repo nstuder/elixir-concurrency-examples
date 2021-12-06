84d09e2329f6b9cd9f82381ae5705700c6380d54
Niklas Studer <niki.studer@web.de>
2021-12-06 09:02:18 +0100

    sort: refactor/reformat File

--- sort/lib/sort.ex
+++ sort/lib/sort.ex
@@ -1,11 +1,12 @@
 defmodule Sort do
-
   def main do
-    number_of_elements = 50000000
-    max_number = 1000000000
+    number_of_elements = 50_000_000
+    max_number = 1_000_000_000
 
     IO.puts("generate Random Numbers")
-    rand_list = 1..number_of_elements
+
+    rand_list =
+      1..number_of_elements
       |> Enum.map(fn _ -> Enum.random(1..max_number) end)
 
     # without Tasks
@@ -17,32 +18,33 @@ defmodule Sort do
     IO.puts("start sorting with Tasks:")
     {time, result} = :timer.tc(Sort, :sort_with_tasks, [rand_list, 2])
     IO.puts("Time: #{time}ns Lenght: #{length(result)}")
-
   end
 
-  def sort(data, index) when index == 0, do: Enum.sort(data)
-  def sort(data, index) do
+  def sort(data, depth) when depth == 0, do: Enum.sort(data)
+
+  def sort(data, depth) do
     # split in to halfs
     {left_half, right_half} = Enum.split(data, div(length(data), 2))
 
     # sort both halfs
-    sorted_left = sort(left_half, index - 1)
-    sorted_right = sort(right_half, index - 1)
+    sorted_left = sort(left_half, depth - 1)
+    sorted_right = sort(right_half, depth - 1)
 
     # merge lists
     :lists.merge(sorted_left, sorted_right)
   end
 
-  def sort_with_tasks(data, index) when index == 0, do: Enum.sort(data)
-  def sort_with_tasks(data, index) do
-    # split in to halfs
+  def sort_with_tasks(data, depth) when depth == 0, do: Enum.sort(data)
+
+  def sort_with_tasks(data, depth) do
+    # split in two halfs
     {left_half, right_half} = Enum.split(data, div(length(data), 2))
 
     # sort both halfs in Task
-    left_task = Task.async(Sort, :sort_with_tasks, [left_half, index - 1])
-    right_task = Task.async(Sort, :sort_with_tasks, [right_half, index - 1])
+    left_task = Task.async(Sort, :sort_with_tasks, [left_half, depth - 1])
+    right_task = Task.async(Sort, :sort_with_tasks, [right_half, depth - 1])
 
     # merge lists
-    :lists.merge(Task.await(left_task, 100000), Task.await(right_task, 100000))
+    :lists.merge(Task.await(left_task, 100_000), Task.await(right_task, 100_000))
   end
 end
