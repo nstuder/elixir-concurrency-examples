9ad3f776f8bb2db963239bbfe66ea60cc5e6c9bc
Niklas Studer <niki.studer@web.de>
2021-12-19 18:02:57 +0100

    add basic Supervisor Example

---
+++ basic/lib/basicSupervisors.ex
@@ -0,0 +1,35 @@
+defmodule BasicSupervisor do
+  def main do
+    children = [
+      %{
+        id: Divide,
+        start: {Divide, :start_link, [[]]}
+      }
+    ]
+
+    {:ok, pid} = Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  def devide(num, to) when num == to, do: :ok
+
+  def devide(num, to) do
+    IO.puts(GenServer.call(Divide, {:devide, num}))
+    devide(num - 1, to)
+  end
+end
+
+defmodule Divide do
+  use GenServer
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  def handle_call({:devide, number}, _from, state) do
+    {:reply, 100 / number, state}
+  end
+end
